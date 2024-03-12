31815c8ee920adb6d65d5c369e11245a8f6c80e7
missymae#2783 <141260118+myCatsName@users.noreply.github.com>
2024-03-12 17:39:36 -0600

    Add useState to keep mute option fresh

--- src/Components/MuteButton.jsx
+++ src/Components/MuteButton.jsx
@@ -1,20 +1,25 @@
 import { FormControl, FormLabel, Switch } from "@chakra-ui/react";
 import { Howler } from "howler";
+import { useState } from "react";
 
 export default function MuteButton() {
+  const [muted, setMuted] = useState(Howler._muted);
+
   const handleChange = () => {
     Howler.mute(!Howler._muted);
+    setMuted((prev) => !prev);
     console.log(`Background Howl.mute(${Howler._muted})`);
   };
 
   return (
     <>
       <FormControl>
-        <FormLabel>Mute Music</FormLabel>
+        <FormLabel>Mute Sound</FormLabel>
         <Switch
           id="mute-music"
           colorScheme="yellow"
           size={"lg"}
+          isChecked={muted}
           onChange={handleChange}
         />
       </FormControl>
