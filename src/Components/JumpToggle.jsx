916a16e5fee3dd95dc48a3f76711ac9c421d9c4b
missymae#2783 <141260118+myCatsName@users.noreply.github.com>
2024-03-13 17:39:13 -0600

    add SFX for options modal and jump toggle

--- src/Components/JumpToggle.jsx
+++ src/Components/JumpToggle.jsx
@@ -1,11 +1,13 @@
 import { FormControl, FormLabel, Switch } from "@chakra-ui/react";
 import { useContext } from "react";
 import GameContext from "../Context/GameContext";
+import { playTamborineSFX } from "../Sound/SFX";
 
 export default function JumpToggle() {
   const { allowJumps, setAllowJumps } = useContext(GameContext);
   function handleChange() {
     setAllowJumps((prev) => !prev);
+    playTamborineSFX();
   }
 
   return (
