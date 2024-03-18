5f9ac3201baa897c1a905ff051788f14d246c15e
missymae#2783 <141260118+myCatsName@users.noreply.github.com>
2024-03-18 16:18:27 -0600

    cleanup Typewriter

--- src/Components/Typewriter.jsx
+++ src/Components/Typewriter.jsx
@@ -1,38 +1,18 @@
 import { useState, useEffect } from "react";
 
-// Define a functional component called Typewriter
-export default function Typewriter({
-  text,
-  delay = 75,
-  pause = 0,
-  cursor = false,
-}) {
-  // with a state variable to track what text has been displayed
+export default function Typewriter({ text, delay = 75, pause = 0 }) {
   const [typedText, setTypedText] = useState("");
 
   useEffect(() => {
     if (text.length === 0) return;
 
     function type(index) {
-      // add a character to the typedText state variable using setTypedText
       setTypedText(text.substring(0, index + 1));
-
-      // If it's the last index, stop typing
       if (index === text.length - 1) return;
-
-      // Otherwise, increment the index and call the type function again
       setTimeout(() => type(index + 1), delay);
     }
-
-    // Start the type function after a pause
     setTimeout(() => type(0), pause);
   }, [text, delay, pause]);
 
-  // Return a span element that displays the typedText state variable with an optional blinking cursor
-  return (
-    <span>
-      {typedText}
-      {cursor && <span className="blinking-cursor">|</span>}
-    </span>
-  );
+  return <span>{typedText}</span>;
 }
