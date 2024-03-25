7be60771cd7afe889c84a4f8c225d5f8bec1a5e6
missymae#2783 <141260118+myCatsName@users.noreply.github.com>
2024-03-25 16:33:22 -0600

    create image preloader component

---
+++ src/HelperFx/Preloader.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+//side panel themes
+import redSwatch from "../Assets/Swatches/background_swatch.png";
+import greenSwatch from "../Assets/Swatches/newGreenSwatch.webp";
+import bloodyeyes from "../Assets/Swatches/bloodyeyes.webp";
+//card back themes
+import card1 from "../Assets/CardFaces/PlayingCardReverse.webp";
+import card2 from "../Assets/CardFaces/EmbossedFlowerCard.webp";
+import card3 from "../Assets/CardFaces/ScrollCard2.webp";
+
+const images = [bloodyeyes, greenSwatch, redSwatch, card1, card2, card3] ;
+
+
+export default function Preloader (){
+
+useEffect(() => {
+    images.forEach((image) => {
+      const imageElement = new Image();
+      imageElement.src = image;
+    })
+  }, []);}
--- src/Layouts/SidePanel.jsx
+++ src/Layouts/SidePanel.jsx
@@ -1,33 +1,21 @@
 import { GridItem } from "@chakra-ui/react";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import ThemeContext from "../Context/ThemeContext";
-import redSwatch from "../Assets/Swatches/background_swatch.png";
-import greenSwatch from "../Assets/Swatches/newGreenSwatch.webp";
-import bloodyeyes from "../Assets/Swatches/bloodyeyes.webp";
-const imagesArray = [bloodyeyes, greenSwatch, redSwatch];
 
 export default function SidePanels() {
   const { currentTheme } = useContext(ThemeContext);
   const sidepanelArt = currentTheme.background;
 
-  //pre-load images to avoid "flicker"
-  useEffect(() => {
-    imagesArray.forEach((image) => {
-      const imageElement = new Image();
-      imageElement.src = image;
-    });
-  }, []);
-
   return (
     <>
       <GridItem
-        className="sidePanel"
+        className="sidepanel"
         backgroundImage={`url(${require("../Assets/Swatches/" +
           sidepanelArt)})`}
         gridArea="1/1/6/4"
       />
       <GridItem
-        className="sidePanel"
+        className="sidepanel"
         backgroundImage={`url(${require("../Assets/Swatches/" +
           sidepanelArt)})`}
         gridArea="1/10/6/13"
