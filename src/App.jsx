7bc2498485a8bec0a1cb89f73b9079d546443718
missymae#2783 <141260118+myCatsName@users.noreply.github.com>
2024-03-26 15:07:37 -0600

    Remove react-router-dom and improve SPA Fix entry modal fade in

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,27 @@
 import "./App.css";
 
 //React
-import React, { lazy, Suspense } from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import React, { useContext, lazy, Suspense } from "react";
 
 // Pages
 import RootLayout from "./Layouts/RootLayout";
 import TitleScreen from "./Layouts/TitleScreen";
+import GameContext from "./Context/GameContext";
 const Preloader = lazy(() => import("./HelperFx/Preloader"));
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<TitleScreen />} />
-      <Route path="main" element={<RootLayout />} />
-      <Route path="*" element={<TitleScreen />} />
-    </Route>
-  )
-);
-
 function App() {
-  window.screen.orientation.lock("landscape").then(
-    (success) => console.log("Orientation locked to landscape", success),
-    (failure) => console.log("Orientation not locked/ not mobile", failure)
-  );
+  const { loadGame } = useContext(GameContext);
+  //TODO: request fullscreen horizontal if mobile
 
-  /*TODO: BUG:
-  Routing in iFrame on itch.io works, but does not load all assets.
-  Check for iFrame, and use a modal if so, or a router in other cases. */
-  if (window.self !== window.top) {
-    console.log("Page is loaded inside an iframe");
-    return (
-      <>
-        <RouterProvider router={router} />
-        <Suspense>
-          <Preloader />
-          <RootLayout />
-        </Suspense>
-      </>
-    );
-  } else {
-    console.log("not framed");
-    return (
-      <>
-        <RouterProvider router={router} />
-        <Suspense>
-          <Preloader />
-        </Suspense>
-      </>
-    );
-  }
+  return (
+    <>
+      <TitleScreen />
+      {loadGame && <RootLayout />}
+      <Suspense>
+        <Preloader />
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
